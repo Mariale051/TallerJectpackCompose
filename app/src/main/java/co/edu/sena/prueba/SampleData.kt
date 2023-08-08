ef884ea670671ea0852289ce4303d82f91c5a70c
Mariale051 <maria051aaguilar@gmail.com>
2023-08-08 09:46:44 -0500

    Add examples files VHDL

---
+++ app/src/main/java/co/edu/sena/prueba/SampleData.kt
@@ -0,0 +1,59 @@
+package co.edu.sena.prueba
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Alejandra",
+            "Where is are you from?"
+        ),
+        Message(
+            "Alejandra",
+            "I'm from the Dominican Republic"
+        ),
+        Message(
+            "Alejandra",
+            "Where are they from?"
+        ),
+        Message(
+            "Alejandra",
+            "They're from Canada"
+        ),
+        Message(
+            "Alejandra",
+            "Are you from Spain"
+        ),
+        Message(
+            "Alejandra",
+            "Yes, I am "
+        ),
+        Message(
+            "Alejandra",
+            "Where's she from"
+        ),
+        Message(
+            "Alejandra",
+            "She's from Tokyo"
+        ),
+        Message(
+            "Alejandra",
+            "Where is he from?"
+        ),
+        Message(
+            "Alejandra",
+            "He's from Peru"
+        ),
+        Message(
+            "Alejandra",
+            "Where are you and you colleague from "
+        ),
+        Message(
+            "Alejandra",
+            "We're from Shanghai"
+        ),
+
+    )
+
+
+}
+
